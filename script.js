294350b557b096a34d683f8ab5fea2c2d3cff301
Jatin Rathee <jatinrathee2018@gmail.com>
2020-04-06 16:06:08 +0530

    first class functions passing functions as arguments

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ var jane = Object.create(personProto, {
     yearOfBirth: { value: 1969},
     job: { value: 'developer'}
 });
-*/
+
 
 // Primitive vs Objects
 
@@ -64,7 +64,7 @@ console.log(a, b);
 
 
 // Objects
-/* Objects properties points to same location */
+//  Objects properties points to same location 
 var obj1 = {
     name: 'John',
     age: 26
@@ -89,3 +89,40 @@ change(age, obj);
 
 console.log(age);
 console.log(obj.city);  
+
+*/
+
+
+// Passing functions as arguments
+
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    for (var i=0; i<arr.length; i++) {
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+function calculateAge(el) {
+    return 2016 - el;
+}
+
+function isFullAge(el) {
+    return el >= 18;
+}
+
+function maxHeartRate(el) {
+    if(el >= 18 && el <= 81) {
+        return Math.round(206.9 - (0.67 * el));
+    } else return -1;
+}
+
+var ages = arrayCalc(years, calculateAge);
+var fullages = arrayCalc(ages, isFullAge);
+var rates = arrayCalc(ages, maxHeartRate);
+
+console.log(ages);
+console.log(fullages);
+console.log(rates);
