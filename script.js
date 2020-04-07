769d63302b9ab53ecfef50531f47bda536df3f4e
Jatin Rathee <jatinrathee2018@gmail.com>
2020-04-07 22:15:27 +0530

    closures

--- script.js
+++ script.js
@@ -171,7 +171,7 @@ function game() {
     console.log(score >= 5);
 }
 game();
-*/
+
 
 (function() {
     var score = Math.random() * 10;
@@ -184,3 +184,41 @@ game();
     var score = Math.random() * 10;
     console.log(score >= 5 - goodLuck);
 })(5);
+*/
+
+
+// Closures => An inner function has always access to the variables and parameters of its outer function, even after the outer function has returned.
+
+function retirement(retirementAge) {
+    var a = ' years left until retirement.';
+    return function (yearOfBirth) {
+        var age = 2016 - yearOfBirth;
+        console.log((retirementAge - age) + a)
+    }
+}
+
+
+var retirementUS = retirement(66);
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
+
+retirementGermany(1990);
+retirementUS(1990);
+retirementIceland(1990);
+
+// retirement(66)(1990);
+
+function interviewQuestion(job) {
+    return function(name) {
+        if (job === 'designer') {
+                console.log(name + ', can you please expain what UX design is?');
+        }
+        else if (job === 'teacher') {
+                console.log('What subject do you teach, ' + name + ' ?');
+        } else {
+                console.log('Hello ' + name + ', what do you do ? ');
+            }
+        }
+}
+
+interviewQuestion('teacher')('Jatin');
